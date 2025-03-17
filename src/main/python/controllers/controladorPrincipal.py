46db9afd84a1ca1b77693132502156d8ec34d2d9
MayiVT <78113457+MayiVT@users.noreply.github.com>
2025-03-17 17:26:06 +0100

    revert commit

--- src/main/python/controllers/controladorPrincipal.py
+++
@@ -1,6 +0,0 @@
-class ControladorPrincipal:
-    def __init__(self):
-        print("Controlador inicialitzat")
-
-    def executar(self):
-        print("Executant aplicació")
--- src/main/python/main.py
+++
@@ -1,5 +0,0 @@
-from controllers.controladorPrincipal import ControladorPrincipal
-
-if __name__ == "__main__":
-    controlador = ControladorPrincipal()
-    controlador.executar()
